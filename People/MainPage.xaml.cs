a1ee2d37f6b17e6b741f7a348e7c5fd2bdeed467
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2020-12-06 22:58:45 +0200

    first commit

---
+++ People/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using People.Models;
+using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+
+
+
+namespace People
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnNewButtonClicked(object sender, EventArgs args)
+        {
+            statusMessage.Text = "";
+
+            App.PersonRepo.AddNewPerson(newPerson.Text, newGender.Text, newTShirtSize.Text, newDateOfOrder.Text, newTShirt_Color.Text, newShipping_Address.Text);
+            statusMessage.Text = App.PersonRepo.StatusMessage;
+        }
+
+        public void OnGetButtonClicked(object sender, EventArgs args)
+        {
+            statusMessage.Text = "";
+
+            List<Person> people = App.PersonRepo.GetAllPeople();
+            peopleList.ItemsSource = people;
+        }
+    }
+}
---
+++ People/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace People.Models
+{
+    [Table("people")]
+    public class Person
+    {
+
+            [PrimaryKey, AutoIncrement]
+            public int Id { get; set; }
+
+            [MaxLength(250), Unique]
+            public string Name { get; set; }
+            public string Gender { get; set; }
+            public string TShirt_Size { get; set; }
+            public string Date_of_Order { get; set; }
+            public string TShirt_Color { get; set; }
+            public string Shipping_Address { get; set; }
+
+
+
+
+    }
+}
